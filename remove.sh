1ccaaf6038f00c1f41dc9f81a9397cafb573873a
Zinedinarnaut <69326871+Zinedinarnaut@users.noreply.github.com>
2021-10-12 18:20:52 +1100

    Changes

---
+++ remove.sh
@@ -0,0 +1,42 @@
+apt update
+echo Packages Updated.
+sleep 1
+apt install iptables-persistent
+/sbin/iptables -t mangle -D PREROUTING -m conntrack --ctstate INVALID -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p icmp -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 224.0.0.0/3 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 169.254.0.0/16 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 172.16.0.0/12 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 192.0.2.0/24 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 192.168.0.0/16 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 10.0.0.0/8 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 0.0.0.0/8 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 240.0.0.0/5 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+/sbin/iptables -t mangle -D PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+/sbin/iptables -D INPUT -p tcp -m connlimit --connlimit-Dbove 111 -j REJECT --reject-with tcp-reset
+/sbin/iptables -D INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+/sbin/iptables -D INPUT -p tcp --tcp-flags RST RST -j DROP
+/sbin/iptables -t mangle -D PREROUTING -f -j DROP
+/sbin/iptables -N port-scanning
+/sbin/iptables -D port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+/sbin/iptables -D port-scanning -j DROP
+/sbin/iptables -D INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+/sbin/iptables -D INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+
+echo "Firewall configuration successfully removed."
